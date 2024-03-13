f984402607f9853eb1cd9ec7c58af0c7304aef14
100JM <bjm_9310@naver.com>
2024-03-13 18:00:39 +0900

    tailwind css

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -15,4 +15,12 @@ const Button = styled.button`
     }
 `
 
-export default Button;
+function TailwindButton({children, ...props}) {
+    return (
+        <button className="px-4 py-2 font-semibold uppercase rounded text-stone-900 bg-amber-400 hover:bg-amber-500" {...props}>
+            {children}
+        </button>
+    )
+}
+
+export default TailwindButton;
